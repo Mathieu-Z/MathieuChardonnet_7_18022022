40bdd0a0534b466b5bffdebea484dc50437cb40d
Mathieu Chardonnet <chardonnet.mathieu@gmail.com>
2022-03-01 12:13:01 +0100

    debut en react

---
+++ Front/src/pages/login.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+// necessaire 2 input (ndc,mdp), un event onclick sur bouton, verif des log (API)
---
+++ Front/src/pages/post.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+/*
+necessite liste des post, trier du plus recent au plus vieux,
+un bouton pour poster, un pour modifier
+*/
